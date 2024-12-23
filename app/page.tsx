4ed2280a53aaa8848cca9af03fbe7536139bfdbf
arunakcm <arunak.comdemetrics@gmail.com>
2024-12-23 15:15:29 +0530

    update

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AdminPanel from "../components/AdminPannel";
 import { Provider } from "react-redux";
 import store from "../redux/store"
-import { useEffect } from "react";
 
 export default function Home() {
 
